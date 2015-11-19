0907e825b63e9092b44723355c5b469c2a5fe9c1
Nouha0 <nouhaghourabi@yahoo.fr>
2015-11-19 12:44:02 +0100

    Update Tabtrie.java

--- src/main/java/tab/Tabtrie.java
+++ src/main/java/tab/Tabtrie.java
@@ -2,29 +2,6 @@ package tab;
 
 
 	public class Tabtrie {
-		public boolean rechercheT(Object v, int tabTrie[])throws Exception{
-			if(!(v instanceof java.lang.Integer)){
-				throw new Exception();
-				
-			}
-			
-			int m = 0;
-			int ifin = tabTrie.length;
-			boolean x = false;
-			int id = 0;
-			while(x == false && (ifin - id) > 1){
-			
-				m = (id + ifin)/2;  //on détermine l'indice de milieu
-			    
-			    x = (tabTrie[m] == (Integer)v);  //on regarde si la valeur recherchée est à cet indice
-			    
-			    if(tabTrie[m] > (Integer)v) ifin = m;  //si la valeur qui est à la case "im" est supérieure à la valeur recherchée, l'indice de fin "ifin" << devient >> l'indice de milieu, ainsi l'intervalle de recherche est restreint lors du prochain tour de boucle
-			    else id = m;  //sinon l'indice de début << devient >> l'indice de milieu et l'intervalle est de la même façon restrein
-				
-				
-		}
-		return x;
-	}
 
 	public void trieC(int tabTrie[]) throws Exception{
 			if( tabTrie.length == 0){
